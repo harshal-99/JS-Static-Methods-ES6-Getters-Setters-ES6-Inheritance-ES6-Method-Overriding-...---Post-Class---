7f9d81f7f9bd17ad00fbba6694d2978a6fe3c7de
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-05-27 12:28:20 +0530

    initial commit

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,17 @@
 // declare class
 
+class Shape {
+  constructor(color) {
+    this.color = color
+  }
+  drawShape() {
+
+  }
+
+  calculateArea() {
+
+  }
+}
+
+module.exports = Shape
 // export class using module.exports
